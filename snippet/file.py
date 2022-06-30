d030e40ef0845b11831b28d02cfffcca83157bc7
Frank Chu <yongfrank@outlook.com>
2022-06-30 12:47:15 +0800

    lab1 donw

---
+++ snippet/file.py
@@ -0,0 +1,74 @@
+from operator import add, sub
+def a_plus_abs_b(a, b):
+    """Return a+abs(b), but without calling abs.
+
+    >>> a_plus_abs_b(2, 3)
+    5
+    >>> a_plus_abs_b(2, -3)
+    5
+    >>> a_plus_abs_b(-1, 4)
+    3
+    >>> a_plus_abs_b(-1, -4)
+    3
+    """
+    if b < 0:
+        f = sub
+    else:
+        f = add
+    return f(a, b)
+
+def largest_factor(n):
+    """Return the largest factor of n that is smaller than n.
+
+    >>> largest_factor(15) # factors are 1, 3, 5
+    5
+    >>> largest_factor(80) # factors are 1, 2, 4, 5, 8, 10, 16, 20, 40
+    40
+    >>> largest_factor(13) # factor is 1 since 13 is prime
+    1
+    """
+    "*** YOUR CODE HERE ***"
+    i = n // 2
+    while(i > 0):
+        if n % i == 0:
+            return i
+        if i == 1:
+            return 1
+
+
+def hailstone(n):
+    """Print the hailstone sequence starting at n and return its
+    length.
+
+    >>> a = hailstone(10)
+    10
+    5
+    16
+    8
+    4
+    2
+    1
+    >>> a
+    7
+    >>> b = hailstone(1)
+    1
+    >>> b
+    1
+    """
+    "*** YOUR CODE HERE ***"
+    if n == 1: 
+        print('1')
+        return 1
+    step = 0
+    while n != 1:
+        print(n)
+        if n % 2 == 0: 
+            n = n // 2
+        else:
+            n = n * 3 + 1
+        step += 1
+    print(n)
+    return step + 1
+    
+a = hailstone(10)
+print(a)
